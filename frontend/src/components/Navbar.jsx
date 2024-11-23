f36a14a6a5d76c400580a3fddf5c4b03762871a3
SUSHANT PATHARE <sushantsp14@gmail.com>
2024-11-23 13:38:51 +0530

    Routes added

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {asssets} from '../assets/frontend_assets/assets'
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between py-5 font-medium'>
+        <img src={ass} alt="" />
+    </div>
+  )
+}
+
+export default Navbar
