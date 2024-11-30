1935b3a962ecc9ae701dea2ff516e5a530e7176b
SUSHANT PATHARE <sushantsp14@gmail.com>
2024-11-30 15:53:44 +0530

    Added NewsLetter Component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
 import OurPolicy from '../components/OurPolicy'
+import NewsLetter from '../components/NewsLetter'
 
 const Home = () => {
 
@@ -12,6 +13,7 @@ const Home = () => {
       <LatestCollection/>
       <BestSeller/>
       <OurPolicy/>
+      <NewsLetter/>
     </div>
   )
 }
