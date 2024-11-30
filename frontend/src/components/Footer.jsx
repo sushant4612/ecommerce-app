5cfd67bbad5e45af5f972d17399a3d2983083dd4
SUSHANT PATHARE <sushantsp14@gmail.com>
2024-11-30 16:04:58 +0530

    Added footer  Component

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
+import { assets } from '../assets/frontend_assets/assets'
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+   <div>
+        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+            <div>
+                <img src={assets.logo} className='mb-5 w-32' alt="" />
+                <p className='w-full md:w-2/3 text-gray-600'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur aperiam rem at error saepe hic a velit possimus voluptates assumenda, vel est veniam nihil reiciendis id odio cumque illum nisi.</p>
+            </div>
+
+            <div>
+                <p className='text-xl font-medium mb-5'>COMPANY</p>
+                <ul className='flex flex-col gap-1 text-gray-600'>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Delivery</li>
+                    <li>Privacy Policy</li>
+                </ul>
+            </div>
+
+            <div>
+                <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+                <ul className='flex flex-col gap-1 text-gray-600'>
+                    <li>+1-212-456-7890</li>
+                    <li>xyz@gmail.com</li>
+                </ul>
+            </div>
+        </div>
+
+        <div>
+            <hr />
+            <p className='py-5 text-sm text-center'>Copyright 2024@ forever.com - All Right reserved</p>
+        </div>
+   </div>
   )
 }
