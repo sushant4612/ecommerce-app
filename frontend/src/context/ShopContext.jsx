0935139d63cb44213dc6b6c52c84454552f99910
SUSHANT PATHARE <sushantsp14@gmail.com>
2024-12-05 13:41:06 +0530

    Added CartItem context

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -9,9 +9,28 @@ const ShopContextProvider = (props) => {
     const delivery_fee = 10;
     const [search , setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
+    const [cartItem, setCartItem] = useState({});
+
+    const addToCart = async (itemId, size) => {
+        let cartData = structuredClone(cartItem);
+
+        if(cartData[itemId]){
+            if(cartData[itemId][size]){
+                cartData[itemId][size] += 1;
+            }else{
+                cartData[itemId][size] = 1;
+            }
+        }
+        else{
+            cartData[itemId] = {};
+            cartData[itemId][size] = 1
+        }
+
+        setCartItem(cartData)
+    }
 
     const value = {
-        products, currency, delivery_fee, search, setSearch, showSearch, setShowSearch
+        products, currency, delivery_fee, search, setSearch, showSearch, setShowSearch, cartItem, addToCart
     }
 
     return (
