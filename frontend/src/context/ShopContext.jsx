d8e78fb6059015b1babf88cd69b2535ed19637d9
SUSHANT PATHARE <sushantsp14@gmail.com>
2024-12-06 15:12:41 +0530

    Added getCartCount functionality

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from '../assets/frontend_assets/assets'
+import { toast } from "react-toastify";
 
 export const ShopContext = createContext();
 
@@ -12,6 +13,11 @@ const ShopContextProvider = (props) => {
     const [cartItem, setCartItem] = useState({});
 
     const addToCart = async (itemId, size) => {
+        if(!size){
+            toast.error("Select Product Size");
+            return
+        }
+
         let cartData = structuredClone(cartItem);
 
         if(cartData[itemId]){
@@ -29,8 +35,24 @@ const ShopContextProvider = (props) => {
         setCartItem(cartData)
     }
 
+    const getCartCount = () => {
+        let totalCount = 0;
+        for(const items in cartItem){
+            for(const item in cartItem[items]){
+                try{
+                    if(cartItem[items][item] > 0){
+                        totalCount += cartItem[items][item];
+                    }
+                }catch(error){
+
+                }
+            }
+        }
+        return totalCount;
+    }
+
     const value = {
-        products, currency, delivery_fee, search, setSearch, showSearch, setShowSearch, cartItem, addToCart
+        products, currency, delivery_fee, search, setSearch, showSearch, setShowSearch, cartItem, addToCart, getCartCount
     }
 
     return (
