7fcdd89b76ad3f8564ecb1c2cdb8daa9f754438d
SUSHANT PATHARE <sushantsp14@gmail.com>
2024-12-07 18:37:06 +0530

    Backed Done

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/frontend_assets/assets'
+import NewsLetter from '../components/NewsLetter'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <div className='text-center text-2xl pt-10 border-t'>
+        <Title text1={'CONTACT'} text2={'US'}/>
+      </div>
+      <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
+        <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
+        <div className='flex flex-col justify-center items-start gap-6'>
+          <p className='font-semibold text-xl text-gray-600'>Our Store</p>
+          <p className='text-gray-500'>5789 Willsons Stattion, <br /> New York</p>
+          <p className='text-gray-500'>Tel: (415) 555-022-333 <br /> Email : admin@xyz.com</p>
+          <p className='text-gray-500'>Careers at Forever</p>
+          <p className='text-gray-500'>Learn more about our teams and job openings</p>
+          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
+        </div>
+      </div>
+      <NewsLetter/>
+    </div>
   )
 }
 
