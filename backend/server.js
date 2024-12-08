174184c31f86d7960134e272f1da73501bb74be2
SUSHANT PATHARE <sushantsp14@gmail.com>
2024-12-08 14:45:30 +0530

    Cloudinary and mongoose connected

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config'
+import connectDb from './config/mongodb.js';
+import cloudinaryConnect from './config/cloudinary.js';
+
+//App Config
+const app = express();
+const port = process.env.PORT || 4000;
+connectDb()
+cloudinaryConnect()
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// api endpoints
+app.get('/', (req, res) => {
+    res.send("API Working")
+})
+
+app.listen(port, () => {
+    console.log(`Server started at port ${port}`);
+});
