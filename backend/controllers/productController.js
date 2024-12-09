bbcc481183ccb810d9646e27658d3f69b95941e9
SUSHANT PATHARE <sushantsp14@gmail.com>
2024-12-09 22:12:23 +0530

    Product Controller Added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -46,19 +46,41 @@ const addProduct = async (req, res) => {
     }
 }
 
-// function for add product
+// function for list product
 const listProducts = async (req, res) => {
-
+    try {
+        const products = await productModel.find({});
+        res.json({success: true, products});
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message})
+    }
 }
 
-// function for add product
+// function for remove product
 const removeProducts = async (req, res) => {
-
+    try {
+        await productModel.findByIdAndDelete(req.body.id);
+        res.json({success : true, message : "Product Removed"})
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message})
+    }
 }
 
-// function for add product
+// function for single product info
 const singleProduct = async (req, res) => {
-
+    try {
+        const { productId } = req.body;
+        console.log("Request Body:", req.body);
+        const product = await productModel.findById(productId);
+        
+        console.log("Requested Product ID:", productId);
+        res.json({success: true, product});
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message})
+    }
 }
 
 export {listProducts, addProduct, removeProducts, singleProduct}
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -6,7 +6,7 @@ const productRouter = express.Router();
 
 productRouter.post('/add',upload.fields([{name:'image1', maxCount:1},{name:'image2', maxCount:1}, {name:'image3', maxCount:1}, {name:'image4', maxCount:1}]), addProduct);
 productRouter.post('/remove', removeProducts);
-productRouter.post('/single', singleProduct);
+productRouter.get('/single', singleProduct);
 productRouter.get('/list', listProducts);
 
 
