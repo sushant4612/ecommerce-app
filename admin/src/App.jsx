b74ef6435a42c3be10401a1efd82c5047fc1aa1d
SUSHANT PATHARE <sushantsp14@gmail.com>
2024-12-10 22:03:31 +0530

    Added Add Componnet half

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -27,7 +27,7 @@ const App = () => {
           <hr />
           <div className='flex w-full'>
             <Sidebar/>
-            <div className='w-[70%] mx-auto m1-[max(5vw,25px) my-8] text-gray-600 text-base'>
+            <div className='w-[70%] mx-auto ml-[max(5vw,25px) my-18] text-gray-600 text-base'>
               <Routes>
                 <Route path='/add' element={<Add  token={token}/>}/>
                 <Route path='/list' element={<List token={token}/>}/>
--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,8 +1,93 @@
 import React from 'react'
+import { assets } from '../assets/admin_assets/assets'
 
 const Add = () => {
   return (
-    <div>Add</div>
+    <form className='flex flex-col w-full items-start gap-3'>
+      <div className='my-4'>
+          <p className='mb-2'>Upload Image</p>
+
+          <div className='flex gap-2'>
+            <label htmlFor="image1">
+              <img className='w-20' src={assets.upload_area} alt="" />
+              <input type="file" id='image1' hidden/>
+            </label>
+            <label htmlFor="image2">
+              <img className='w-20' src={assets.upload_area} alt="" />
+              <input type="file" id='image2' hidden/>
+            </label>
+            <label htmlFor="image3">
+              <img className='w-20' src={assets.upload_area} alt="" />
+              <input type="file" id='image3' hidden/>
+            </label>
+            <label htmlFor="image4">
+              <img className='w-20' src={assets.upload_area} alt="" />
+              <input type="file" id='image4' hidden/>
+            </label>
+          </div>
+      </div>
+
+      <div className='w-full'>
+        <p className='mb-2'>Product name</p>
+        <input className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Type here' required/>
+      </div>
+   
+      <div className='w-full'>
+        <p className='mb-2'>Product description</p>
+        <textarea className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Write content here' required/>
+      </div>
+
+
+  <div className='flex flex-col sm:flex-row gap-2 w-full sm:gap-8'>
+      <div>
+        <p className='mb-2'>Product category</p>
+        <select className='w-full px-3 py-2'>
+          <option value="Men">Men</option>
+          <option value="Women">Women</option>
+          <option value="Kids">Kids</option>
+        </select>
+      </div>
+
+      <div>
+        <p className='mb-2'>Sub category</p>
+        <select className='w-full px-3 py-2'>
+          <option value="Topwear">Topwear</option>
+          <option value="Bottomwear">Bottomwear</option>
+          <option value="Winterwear">Winterwear</option>
+        </select>
+      </div>
+
+      <div>
+        <p className='mb-2'>Product Price</p>
+        <input className='w-full px-3 py-2 sm:w-[120px]' type="Number"  placeholder='25'/>
+      </div>
+    </div>
+    <div>
+      <p className='mb-2'>Product Sizes</p>
+      <div className='flex gap-3'>
+        <div>
+          <p className='bg-slate-200 px-3 py-1 cursor-pointer'>S</p>
+        </div>
+        <div>
+          <p className='bg-slate-200 px-3 py-1 cursor-pointer'>M</p>
+        </div>
+        <div>
+          <p className='bg-slate-200 px-3 py-1 cursor-pointer'>L</p>
+        </div>
+        <div>
+          <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XL</p>
+        </div>
+        <div>
+          <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XXL</p>
+        </div>
+      </div>
+    </div>
+
+    <div className='flex gap-2 mt-2'>
+      <input type="checkbox" id='bestseller'/>
+      <label className='cursor-pointer' htmlFor="bestseller">Add to bestseller</label>
+    </div>
+    </form>
   )
 }
 
