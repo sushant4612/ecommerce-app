0ae4607b3970ac47b927effda411702b92b48004
SUSHANT PATHARE <sushantsp14@gmail.com>
2024-12-13 20:43:57 +0530

    Order Model Added

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,21 @@
+
+
+// Placing order using COD method
+const placeOrder = async (req, res) => {
+
+}
+
+// Placing order using Stripe method
+const placeOrderStripe = async (req, res) => {
+
+}
+
+// Placing order using Razorpay method
+const placeOrderRazorpay = async (req, res) => {
+
+}
+
+// All Orders Data for Admin Panel
+const placeOrderRazorpay = async (req, res) => {
+
+}
