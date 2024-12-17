4933ec6eecc598d69ff806ea600dc8dfc68d8907
SUSHANT PATHARE <sushantsp14@gmail.com>
2024-12-17 18:55:32 +0530

    Added Frontend Logic for Order Status

--- admin/src/pages/Order.jsx
+++ admin/src/pages/Order.jsx
@@ -26,6 +26,18 @@ const Order = ({token}) => {
     }
   }
 
+  const statusHandler = async ( event, orderId ) => {
+    try {
+      const response = await axios.post(backendUrl + '/api/order/status', {orderId, status: event.target.value}, {headers: {token}})
+      if(response.data.success){
+        await fetchAllOrdeers()
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(response.data.message)
+    }
+  }
+
   useEffect(() => {
     fetchAllOrdeers();
   },[token])
@@ -64,7 +76,7 @@ const Order = ({token}) => {
               <p>Date: {new Date(order.date).toLocaleDateString()}</p>
             </div>
               <p className='text-sm sm:text-[15px]'>{currency} {order.amount}</p>
-              <select value={order.status} className='p-2 font-semibold'>
+              <select onChange={(e) => statusHandler(e, order._id) } value={order.status} className='p-2 font-semibold'>
                 <option value="Order Placed">Order Placed</option>
                 <option value="Packing">Packing</option>
                 <option value="Shipped">Shipped</option>
