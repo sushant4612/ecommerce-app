980358c91152b713bb38f9e4ca26ab9ddc935ccc
SUSHANT PATHARE <sushantsp14@gmail.com>
2024-12-17 19:26:51 +0530

    Stripe Payment Backend Added

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,14 @@
+import { currency } from '../../admin/src/App.jsx';
 import orderModel from '../models/orderModel.js'
 import userModel from '../models/userModel.js'
+import Stripe from 'stripe'
+
+// global variables
+const currency = 'inr'
+const deliverCharges = 10
+
+// gateway initialize
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // Placing order using COD method
 const placeOrder = async (req, res) => {
@@ -30,7 +39,59 @@ const placeOrder = async (req, res) => {
 
 // Placing order using Stripe method
 const placeOrderStripe = async (req, res) => {
+    try {
+        const { userId, items, amount, address } = req.body;
+        const { origin } = req.headers;
 
+        const orderData = {
+            userId,
+            items,
+            amount,
+            address,
+            paymentMethod: "Stripe",
+            payment:false,
+            date: Date.now()
+        }
+
+        const newOrder = new orderModel(orderData);
+        await newOrder.save();
+
+        const line_items = items.map((item) => ({
+            price_data: {
+                currency,
+                product_data: {
+                    name: item.name
+                },
+                unit_amount: item.price * 100
+            },
+            quantity: item.quantity
+        }))
+        
+        line_items.push(
+            {
+                price_data: {
+                    currency,
+                    product_data: {
+                        name: 'Delivery Charges'
+                    },
+                    unit_amount: deliverCharges * 100
+                },
+                quantity: 1
+            }
+        )
+
+        const session = await stripe.checkout.sessions.create({
+            success_url: `${origin}/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url: `${origin}/verify?success=false&orderId=${newOrder._id}`,
+            line_items,
+            mode: 'payment',
+        })
+
+        res.json({success: true, session_url: session.url})
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message})
+    }
 }
 
 // Placing order using Razorpay method
